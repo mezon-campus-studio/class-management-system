d182f256a28cd547e92939236bdbe30e0895a592
Bui_The_Son <buitheson201@gmail.com>
2026-04-13 15:44:25 +0700

    fix: big size font theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -189,20 +189,20 @@ module.exports = {
 
 			fontSize: {
 				// format: [size, { lineHeight, letterSpacing? }]
-				"2xs": ["0.6rem", { lineHeight: "1rem" }],
-				xs: ["0.72rem", { lineHeight: "1.1rem" }],
-				sm: ["0.82rem", { lineHeight: "1.35rem" }],
-				base: ["0.875rem", { lineHeight: "1.65rem" }],   // 14px
-				md: ["0.9375rem", { lineHeight: "1.6rem" }],    // 15px
-				lg: ["1rem", { lineHeight: "1.5rem" }],    // 16px
-				xl: ["1.125rem", { lineHeight: "1.4rem" }],    // 18px
-				"2xl": ["1.25rem", { lineHeight: "1.3rem" }],    // 20px
-				"3xl": ["1.5rem", { lineHeight: "1.25rem" }],   // 24px
-				"4xl": ["1.75rem", { lineHeight: "1.2rem" }],    // 28px
-				"5xl": ["2rem", { lineHeight: "1.15rem" }],   // 32px
-				"display-sm": ["1.5rem", { lineHeight: "1.2rem", letterSpacing: "-0.03em" }],
-				"display-md": ["1.75rem", { lineHeight: "1.15rem", letterSpacing: "-0.035em" }],
-				"display-lg": ["2rem", { lineHeight: "1.1rem", letterSpacing: "-0.04em" }],
+				"2xs": ["0.65rem", { lineHeight: "1rem" }],
+				xs: ["0.75rem", { lineHeight: "1.125rem" }],
+				sm: ["0.875rem", { lineHeight: "1.25rem" }],
+				base: ["1rem", { lineHeight: "1.5rem" }],
+				md: ["1.0625rem", { lineHeight: "1.6rem" }],
+				lg: ["1.125rem", { lineHeight: "1.75rem" }],
+				xl: ["1.25rem", { lineHeight: "1.75rem" }],
+				"2xl": ["1.5rem", { lineHeight: "2rem" }],
+				"3xl": ["1.875rem", { lineHeight: "2.25rem" }],
+				"4xl": ["2.25rem", { lineHeight: "2.5rem" }],
+				"5xl": ["3rem", { lineHeight: "1" }],
+				"display-sm": ["2.25rem", { lineHeight: "2.5rem", letterSpacing: "-0.03em" }],
+				"display-md": ["3rem", { lineHeight: "1", letterSpacing: "-0.035em" }],
+				"display-lg": ["3.75rem", { lineHeight: "1", letterSpacing: "-0.04em" }],
 			},
 
 			fontWeight: {
